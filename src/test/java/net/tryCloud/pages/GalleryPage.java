0b7082173d1319470c491bc31f002d355c1a1542
mutlubilge <kanokasale@gmail.com>
2021-09-27 20:52:25 +0300

    Galery Page is created

--- src/test/java/net/tryCloud/pages/GalleryPage.java
+++ src/test/java/net/tryCloud/pages/GalleryPage.java
@@ -1,4 +1,19 @@
 package net.tryCloud.pages;
 
-public class GalleryPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+
+
+public class GalleryPage extends BasePage{
+
+    @FindBy(xpath = "//a[@class='button new']/span[1]")
+    public WebElement addPicture;
+
+    @FindBy(xpath = "//div[@class='newFileMenu popovermenu bubble menu open menu-left']/ul/li/label/span[1]")
+    public WebElement upload;
+
+
+
+
 }
