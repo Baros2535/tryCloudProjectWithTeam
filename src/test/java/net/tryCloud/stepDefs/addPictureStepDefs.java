0018cf9da5b12a4aba435f5f165e3149afeca9fe
mutlubilge <kanokasale@gmail.com>
2021-09-27 21:29:00 +0300

    addPicture.feature and addPictureStepDefs are created

---
+++ src/test/java/net/tryCloud/stepDefs/addPictureStepDefs.java
@@ -0,0 +1,27 @@
+package net.tryCloud.stepDefs;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class addPictureStepDefs {
+    @Given("The user is already logged in")
+    public void theUserIsAlreadyLoggedIn() {
+    }
+
+    @Then("The user clicks to gallery symbol on the header")
+    public void theUserClicksToGallerySymbolOnTheHeader() {
+    }
+
+    @Then("The user cliks to plus icon")
+    public void theUserCliksToPlusIcon() {
+    }
+
+    @Then("The user sends FilePath address of image to upload icon")
+    public void theUserSendsFilePathAddressOfImageToUploadIcon() {
+    }
+
+    @And("original image name must be the same with uploaded image")
+    public void originalImageNameMustBeTheSameWithUploadedImage() {
+    }
+}
