fc9bae8254f8f64a17e20e5dc46d3a120a25e746
selimoz <y.selimoz@hotmail.com>
2021-09-28 11:32:39 +0100

    duzeltildi variable

--- src/test/java/net/tryCloud/pages/BasePage.java
+++ src/test/java/net/tryCloud/pages/BasePage.java
@@ -3,14 +3,37 @@ package net.tryCloud.pages;
 import net.tryCloud.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class BasePage {
     public BasePage() {
         PageFactory.initElements( Driver.get(), this);
     }
 
-@FindBy(xpath = "(//a[@href='/index.php/apps/files/'])[1]")
+    //burak commit
+    @FindBy(css = "#appmenu>[data-id]")
+    public List<WebElement> appMenu;
+
+    public WebElement fileMenu = appMenu.get(0);
+    public WebElement activityMenu = appMenu.get(1);
+    public WebElement galleryMenu = appMenu.get(2);
+    public WebElement talkMenu = appMenu.get(3);
+    public WebElement calendarMenu = appMenu.get(4);
+    public WebElement deckMenu = appMenu.get(5);
+    public WebElement announcementsMenu = appMenu.get(6);
+
+    @FindBy(css = "#expanddiv [data-id]")
+    public List<WebElement> avatarMenu;
+
+    public WebElement settingsIcon = avatarMenu.get(0);
+    public WebElement helpIcon = avatarMenu.get(1);
+    public WebElement logoutIcon = avatarMenu.get(2);
+
+
+/*@FindBy(xpath = "(//a[@href='/index.php/apps/files/'])[1]")
     public WebElement fileMenu;
 
 @FindBy(xpath = "(//a[@aria-label='Activity'])[1]")
@@ -29,7 +52,7 @@ public class BasePage {
     public WebElement deckMenu;
 
 @FindBy(xpath = "(//a[@aria-label='Announcements'])[1]")
-    public WebElement announcementsMenu;
+    public WebElement announcementsMenu;*/
 
 @FindBy(xpath = "(//div[@tabindex='0'])[1]")
     public WebElement notifications;
@@ -40,11 +63,13 @@ public class BasePage {
 @FindBy(xpath = "//div[@class='avatardiv avatardiv-shown']")
     public WebElement avatarIcon;
 
-@FindBy(xpath = "//li[@data-id='settings']/a")
+/*@FindBy(xpath = "//li[@data-id='settings']/a")
     public WebElement settingsIcon;
 
 @FindBy(xpath = "//li[@data-id='logout']/a")
-    public WebElement logoutIcon;
+    public WebElement logoutIcon;*/
+
+
 
 
 
